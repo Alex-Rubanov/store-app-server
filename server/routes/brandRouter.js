8114e9600ab8f89fb3677c8a204b449ef0e8d002
Alex <rubanovv.alex@gmail.com>
2023-04-04 13:30:58 +0200

    added basic jwt auth with user and role check

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express');
 const router = new Router();
 const brandController = require('../controllers/brandController');
+const roleCheck = require('../middleware/checkRoleMiddleware');
 
-router.post('/', brandController.create);
+router.post('/', roleCheck('ADMIN'), brandController.create);
 router.get('/', brandController.getAll);
 
 module.exports = router;
